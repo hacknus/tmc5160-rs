91ddcdead177a694b3d435b517049ee7aaf2a2ed
hacknus <linus.stoeckli@unibe.ch>
2025-10-14 18:02:15 +0200

    add reading of ioin register
    (cherry picked from commit 3b30fa38a25df0ed0626b6049764d509b5bc88d8)

--- src/lib.rs
+++ src/lib.rs
@@ -541,6 +541,13 @@ where
         Ok(RampStat::from_bytes(packet.data.to_le_bytes()))
     }
 
+    /// read IoIn register
+    pub fn read_ioin(&mut self) -> Result<IoIn, Error<E>> {
+        let packet = self.read_register(Registers::IOIN)?;
+        self.status = packet.status;
+        Ok(IoIn::from_bytes(packet.data.to_le_bytes()))
+    }
+
     /// read ENC_STATUS register
     pub fn read_enc_status(&mut self) -> Result<EncStatus, Error<E>> {
         let packet = self.read_register(Registers::ENC_STATUS)?;
